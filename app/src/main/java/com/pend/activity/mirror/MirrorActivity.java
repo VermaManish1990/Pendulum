2e2e2c3b3c4e7b9beae2f240a9de4653eb435448
shubh <shubham.chauhan@kelltontech.com>
2018-06-04 14:07:27 +0530

    Initial commit

---
+++ app/src/main/java/com/pend/activity/mirror/MirrorActivity.java
@@ -0,0 +1,35 @@
+package com.pend.activity.mirror;
+
+import android.os.Bundle;
+
+import com.pend.BaseActivity;
+import com.pend.R;
+
+public class MirrorActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mirror);
+    }
+
+    @Override
+    public void updateUi(boolean status, int actionID, Object serviceResponse) {
+
+    }
+
+    @Override
+    public void onEvent(int eventId, Object eventData) {
+
+    }
+
+    @Override
+    public void getData(int actionID) {
+
+    }
+
+    @Override
+    public void onAuthError() {
+
+    }
+}
