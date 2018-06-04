a2d9ad8770042ff5c3c815af581c3dc1f3c97ca3
shubh <shubham.chauhan@kelltontech.com>
2018-06-04 15:26:56 +0530

    Initial commit

--- app/src/main/java/com/pend/activity/mirror/MirrorActivity.java
+++ app/src/main/java/com/pend/activity/mirror/MirrorActivity.java
@@ -1,16 +1,34 @@
 package com.pend.activity.mirror;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.view.View;
 
 import com.pend.BaseActivity;
 import com.pend.R;
 
 public class MirrorActivity extends BaseActivity {
 
+    private View mRootView;
+    private ViewPager mViewPagerMirror;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mirror);
+
+        initUI();
+    }
+
+    @Override
+    protected void initUI() {
+
+        mRootView = findViewById(R.id.root_view);
+        TabLayout tabLayoutMirror = findViewById(R.id.tab_layout_mirror);
+        mViewPagerMirror = findViewById(R.id.view_pager_mirror);
+
+        tabLayoutMirror.setupWithViewPager(mViewPagerMirror, true);
     }
 
     @Override
---
+++ app/src/main/java/com/pend/adapters/MirrorViewPagerAdapter.java
@@ -0,0 +1,22 @@
+package com.pend.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MirrorViewPagerAdapter extends FragmentPagerAdapter{
+
+    public MirrorViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
