472d0bd244bb67d484df94d076c43762713492e0
shubh <shubham.chauhan@kelltontech.com>
2018-06-06 23:14:08 +0530

    Initial commit

--- app/src/main/java/com/pend/activity/mirror/ReflectionsActivity.java
+++ app/src/main/java/com/pend/activity/mirror/ReflectionsActivity.java
@@ -1,21 +1,87 @@
 package com.pend.activity.mirror;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 import com.pend.BaseActivity;
 import com.pend.R;
+import com.pend.adapters.ReflectionMirrorAdapter;
+import com.pend.interfaces.IApiEvent;
+import com.pend.interfaces.IWebServices;
+import com.pend.models.GetReflectionUsersResponseModel;
+import com.pend.util.LoggerUtil;
+import com.pend.util.NetworkUtil;
+import com.pend.util.VolleyErrorListener;
+import com.pendulum.utils.ConnectivityUtils;
+import com.pendulum.volley.ext.GsonObjectRequest;
+import com.pendulum.volley.ext.RequestManager;
+
+import java.util.ArrayList;
 
 public class ReflectionsActivity extends BaseActivity {
+
+    private static final String TAG = ReflectionsActivity.class.getSimpleName();
+    private View mRootView;
+    private GridView mGridViewReflection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reflections);
+
+        initUI();
+        setInitialData();
+    }
+
+    @Override
+    protected void initUI() {
+
+        mRootView = findViewById(R.id.root_view);
+        mGridViewReflection = findViewById(R.id.grid_view_reflection);
+    }
+
+    @Override
+    protected void setInitialData() {
+
+        ArrayList<GetReflectionUsersResponseModel.GetReflectionUsersDetails> mirrorList = new ArrayList<>();
+        mGridViewReflection.setAdapter(new ReflectionMirrorAdapter(this, mirrorList));
+
+        /*
+          On Click event for Single Gridview Item
+          */
+        mGridViewReflection.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+            }
+        });
     }
 
     @Override
     public void updateUi(boolean status, int actionID, Object serviceResponse) {
+        switch (actionID) {
+            case IApiEvent.REQUEST_GET_REFLECTION_USERS_CODE:
+                if (status) {
+                    GetReflectionUsersResponseModel getReflectionUsersResponseModel = (GetReflectionUsersResponseModel) serviceResponse;
+                    if (getReflectionUsersResponseModel != null && getReflectionUsersResponseModel.status) {
+                        LoggerUtil.d(TAG, getReflectionUsersResponseModel.statusCode);
 
+                    } else {
+                        LoggerUtil.d(TAG, getString(R.string.server_error_from_api));
+                    }
+                } else {
+                    LoggerUtil.d(TAG, getString(R.string.status_is_false));
+                }
+                break;
+
+            default:
+                LoggerUtil.d(TAG, getString(R.string.wrong_case_selection));
+                break;
+        }
+        removeProgressDialog();
     }
 
     @Override
@@ -24,8 +90,32 @@ public class ReflectionsActivity extends BaseActivity {
     }
 
     @Override
-    public void getData(int actionID) {
+    public void getData(final int actionID) {
+        if (!ConnectivityUtils.isNetworkEnabled(this)) {
+            Snackbar.make(mRootView, getString(R.string.no_internet_connection), Snackbar.LENGTH_LONG);
+            return;
+        }
+        showProgressDialog();
 
+        switch (actionID) {
+            case IApiEvent.REQUEST_GET_REFLECTION_USERS_CODE:
+
+                RequestManager.addRequest(new GsonObjectRequest<GetReflectionUsersResponseModel>(IWebServices.REQUEST_GET_REFLECTION_USERS_URL,
+                        NetworkUtil.getHeaders(this), null, GetReflectionUsersResponseModel.class,
+                        new VolleyErrorListener(this, actionID)) {
+
+                    @Override
+                    protected void deliverResponse(GetReflectionUsersResponseModel response) {
+                        updateUi(true, actionID, response);
+
+                    }
+                });
+                break;
+
+            default:
+                LoggerUtil.d(TAG, getString(R.string.wrong_case_selection));
+                break;
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/pend/adapters/ReflectionMirrorAdapter.java
@@ -0,0 +1,76 @@
+package com.pend.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.pend.R;
+import com.pend.models.GetReflectionUsersResponseModel;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ReflectionMirrorAdapter extends BaseAdapter {
+
+    private final ArrayList<GetReflectionUsersResponseModel.GetReflectionUsersDetails> mUserDataList;
+    private final Context mContext;
+
+    public ReflectionMirrorAdapter(Context context, ArrayList<GetReflectionUsersResponseModel.GetReflectionUsersDetails> mirrorList) {
+        mContext = context;
+        mUserDataList = mirrorList;
+    }
+
+    @Override
+    public int getCount() {
+        return mUserDataList != null ? mUserDataList.size() : 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mUserDataList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View grid;
+        if (convertView == null) {
+            grid = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_reflection_mirror_item, parent, false);
+        } else {
+            grid = convertView;
+        }
+
+        bindData(grid, position);
+
+        return grid;
+    }
+
+    /**
+     * Method is used to bind data with UI.
+     *
+     * @param view     view
+     * @param position position
+     */
+    private void bindData(View view, int position) {
+        GetReflectionUsersResponseModel.GetReflectionUsersDetails reflectionUsersDetails = mUserDataList.get(position);
+
+        View rootView = view.findViewById(R.id.root_view);
+        TextView tvName = view.findViewById(R.id.tv_name);
+        ImageView ivMessage = view.findViewById(R.id.iv_message);
+        ImageView ivProfile = view.findViewById(R.id.iv_profile);
+
+        tvName.setText(reflectionUsersDetails.userFullName != null ? reflectionUsersDetails.userFullName : "");
+//        Picasso.with(mContext)
+//                .load("")
+//                .resize(360,360)
+//                .into(ivProfile);
+    }
+}
