62166f24af71de284d21180e253e2ef9921b1213
shubh <shubham.chauhan@kelltontech.com>
2018-06-13 17:55:01 +0530

    Initial commit

--- app/src/main/java/com/pend/adapters/ExitPollAdapter.java
+++ app/src/main/java/com/pend/adapters/ExitPollAdapter.java
@@ -2,7 +2,6 @@ package com.pend.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
