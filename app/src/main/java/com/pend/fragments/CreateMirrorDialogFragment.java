c5a25c36598bc6d9749c88bc1f26fd7df4e8be74
shubh <shubham.chauhan@kelltontech.com>
2018-06-14 01:22:28 +0530

    Initial commit

---
+++ app/src/main/java/com/pend/fragments/CreateMirrorDialogFragment.java
@@ -0,0 +1,21 @@
+package com.pend.fragments;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pend.R;
+
+public class CreateMirrorDialogFragment extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_search_mirror, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/pend/fragments/UnVotingDialogFragment.java
@@ -0,0 +1,21 @@
+package com.pend.fragments;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pend.R;
+
+public class UnVotingDialogFragment extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_unvote_popup, container, false);
+
+        return view;
+    }
+}
