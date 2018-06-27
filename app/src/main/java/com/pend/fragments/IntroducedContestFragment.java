80dc06bd6a176d9f3e2408cba34d202b1e30c389
shubh <shubham.chauhan@kelltontech.com>
2018-06-27 13:52:54 +0530

    Initial commit

--- app/src/main/java/com/pend/fragments/IntroducedContestFragment.java
+++ app/src/main/java/com/pend/fragments/IntroducedContestFragment.java
@@ -1,8 +1,13 @@
 package com.pend.fragments;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,6 +18,8 @@ import android.widget.TextView;
 import com.pend.BaseFragment;
 import com.pend.R;
 import com.pend.activity.contest.CreateContestType1Activity;
+import com.pend.activity.contest.CreateContestType2Activity;
+import com.pend.interfaces.Constants;
 import com.pend.util.LoggerUtil;
 
 public class IntroducedContestFragment extends BaseFragment implements View.OnClickListener {
@@ -81,13 +88,44 @@ public class IntroducedContestFragment extends BaseFragment implements View.OnCl
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt_create_contest:
-                Intent intent = new Intent(mContext, CreateContestType1Activity.class);
-                startActivity(intent);
+                selectContestDialog();
                 break;
 
             default:
                 LoggerUtil.d(TAG, getString(R.string.wrong_case_selection));
                 break;
         }
+    }
+
+    /**
+     * Method is used to show an Alert Dialog for select Contest.
+     */
+    private void selectContestDialog() {
+        final CharSequence[] items = new String[]{getString(R.string.type_1_contest), getString(R.string.type_2_contest), getString(R.string.cancel)};
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setTitle(getString(R.string.add_photo));
+
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (items[which].toString()) {
+                    case "TYPE 1 Contest":
+                        Intent intentContest1 = new Intent(mContext, CreateContestType1Activity.class);
+                        startActivity(intentContest1);
+                        break;
+
+                    case "TYPE 2 Contest":
+                        Intent intentContest2 = new Intent(mContext, CreateContestType2Activity.class);
+                        startActivity(intentContest2);
+                        break;
+
+                    case "Cancel":
+                        dialog.dismiss();
+                        break;
+                }
+            }
+        });
+        builder.show();
     }
 }
