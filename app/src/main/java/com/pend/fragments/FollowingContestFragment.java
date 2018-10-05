9d3c055afa860c02bc9b34cc1e1d5eeab8345ba7
shubh <shubham.chauhan@kelltontech.com>
2018-10-05 17:20:10 +0530

    + add api for contest.

--- app/src/main/java/com/pend/fragments/FollowingContestFragment.java
+++ app/src/main/java/com/pend/fragments/FollowingContestFragment.java
@@ -2,6 +2,7 @@ package com.pend.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,16 +14,33 @@ import com.pend.BaseActivity;
 import com.pend.BaseFragment;
 import com.pend.R;
 import com.pend.adapters.ContestAdapter;
+import com.pend.interfaces.Constants;
+import com.pend.interfaces.IApiEvent;
+import com.pend.interfaces.IWebServices;
 import com.pend.models.ContestResponseModel;
+import com.pend.models.GetContestsResponseModel;
+import com.pend.util.LoggerUtil;
+import com.pend.util.NetworkUtil;
+import com.pend.util.OtherUtil;
+import com.pend.util.SharedPrefUtils;
+import com.pend.util.VolleyErrorListener;
+import com.pendulum.utils.ConnectivityUtils;
+import com.pendulum.volley.ext.GsonObjectRequest;
+import com.pendulum.volley.ext.RequestManager;
 
 import java.util.ArrayList;
 
 public class FollowingContestFragment extends BaseFragment {
 
+    private static final String TAG = FollowingContestFragment.class.getSimpleName();
     private View mRootView;
     private TextView mTvDataNotAvailable;
     private RecyclerView mRecyclerViewFollowing;
     private BaseActivity mContext;
+    private int mPageNumber;
+    private String mSearchText;
+    private boolean mIsHasNextPage;
+    private boolean mIsLoading;
 
     @Override
     public void onAttach(Context context) {
@@ -52,6 +70,11 @@ public class FollowingContestFragment extends BaseFragment {
     @Override
     protected void setInitialData() {
 
+        mSearchText = "";
+        mPageNumber = 1;
+        mIsLoading = false;
+        mIsHasNextPage = false;
+
         mRecyclerViewFollowing.setVisibility(View.VISIBLE);
         mTvDataNotAvailable.setVisibility(View.GONE);
         ArrayList<ContestResponseModel.ContestDetails> contestDetailsList = new ArrayList<>();
@@ -70,7 +93,48 @@ public class FollowingContestFragment extends BaseFragment {
 
     @Override
     public void updateUi(boolean status, int actionID, Object serviceResponse) {
+        switch (actionID) {
+            case IApiEvent.REQUEST_GET_FOLLOWING_CONTESTS_URL:
+                if (status) {
+                    GetContestsResponseModel contestsResponseModel =
+                            (GetContestsResponseModel) serviceResponse;
+                    if (contestsResponseModel != null && contestsResponseModel.status) {
+                        LoggerUtil.d(TAG, contestsResponseModel.statusCode);
 
+                        if (contestsResponseModel.Data != null && contestsResponseModel.Data.contestList != null) {
+
+                            mTvDataNotAvailable.setVisibility(View.GONE);
+                            mRecyclerViewFollowing.setVisibility(View.VISIBLE);
+
+                            mIsHasNextPage = !contestsResponseModel.Data.hasNextPage;
+
+                        } else {
+                            mTvDataNotAvailable.setVisibility(View.VISIBLE);
+                            mRecyclerViewFollowing.setVisibility(View.GONE);
+                        }
+                    } else {
+                        LoggerUtil.d(TAG, getString(R.string.server_error_from_api));
+                        mTvDataNotAvailable.setVisibility(View.VISIBLE);
+                        mRecyclerViewFollowing.setVisibility(View.GONE);
+                    }
+                } else {
+                    LoggerUtil.d(TAG, getString(R.string.status_is_false));
+                    mTvDataNotAvailable.setVisibility(View.VISIBLE);
+                    mRecyclerViewFollowing.setVisibility(View.GONE);
+                }
+
+                mIsLoading = false;
+                break;
+
+            default:
+                LoggerUtil.d(TAG, getString(R.string.wrong_case_selection));
+                break;
+        }
+
+        if (mContext != null && !mContext.isDestroyed() && !mContext.isFinishing() && isAdded()) {
+
+            mContext.removeProgressDialog();
+        }
     }
 
     @Override
@@ -80,7 +144,49 @@ public class FollowingContestFragment extends BaseFragment {
 
     @Override
     public void getData(int actionID) {
+        if (mContext != null && !mContext.isDestroyed() && !mContext.isFinishing() && isAdded()) {
 
+            if (!ConnectivityUtils.isNetworkEnabled(mContext)) {
+                Snackbar.make(mRootView, getString(R.string.no_internet_connection), Snackbar.LENGTH_LONG).show();
+                return;
+            }
+            mContext.showProgressDialog();
+        } else {
+            return;
+        }
+
+        switch (actionID) {
+            case IApiEvent.REQUEST_GET_FOLLOWING_CONTESTS_URL:
+
+                String trendingMirrorUrl;
+                if (mSearchText != null) {
+
+                    mSearchText = OtherUtil.replaceWithPattern(mSearchText, " ");
+                    mSearchText = mSearchText.replaceAll(" ", "%20");
+
+                    trendingMirrorUrl = IWebServices.REQUEST_GET_FOLLOWING_CONTESTS_URL + Constants.PARAM_USER_ID + "=" + SharedPrefUtils.getUserId(mContext)
+                            + "&" + Constants.PARAM_PAGE_NUMBER + "=" + String.valueOf(mPageNumber)
+                            + "&" + Constants.PARAM_SEARCH_TEXT + "=" + mSearchText;
+                } else {
+                    trendingMirrorUrl = IWebServices.REQUEST_GET_FOLLOWING_CONTESTS_URL + Constants.PARAM_USER_ID + "=" + SharedPrefUtils.getUserId(mContext)
+                            + "&" + Constants.PARAM_PAGE_NUMBER + "=" + String.valueOf(mPageNumber);
+                }
+
+                RequestManager.addRequest(new GsonObjectRequest<GetContestsResponseModel>(trendingMirrorUrl,
+                        NetworkUtil.getHeaders(mContext), null, GetContestsResponseModel.class,
+                        new VolleyErrorListener(this, actionID)) {
+
+                    @Override
+                    protected void deliverResponse(GetContestsResponseModel response) {
+                        updateUi(true, actionID, response);
+                    }
+                });
+                break;
+
+            default:
+                LoggerUtil.d(TAG, getString(R.string.wrong_case_selection));
+                break;
+        }
     }
 
     @Override
--- app/src/main/java/com/pend/fragments/IntroducedContestFragment.java
+++ app/src/main/java/com/pend/fragments/IntroducedContestFragment.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -22,8 +23,18 @@ import com.pend.activity.contest.CreateContestType1Activity;
 import com.pend.activity.contest.CreateContestType2Activity;
 import com.pend.adapters.ContestAdapter;
 import com.pend.interfaces.Constants;
+import com.pend.interfaces.IApiEvent;
+import com.pend.interfaces.IWebServices;
 import com.pend.models.ContestResponseModel;
+import com.pend.models.GetContestsResponseModel;
 import com.pend.util.LoggerUtil;
+import com.pend.util.NetworkUtil;
+import com.pend.util.OtherUtil;
+import com.pend.util.SharedPrefUtils;
+import com.pend.util.VolleyErrorListener;
+import com.pendulum.utils.ConnectivityUtils;
+import com.pendulum.volley.ext.GsonObjectRequest;
+import com.pendulum.volley.ext.RequestManager;
 
 import java.util.ArrayList;
 
@@ -34,6 +45,10 @@ public class IntroducedContestFragment extends BaseFragment implements View.OnCl
     private TextView mTvDataNotAvailable;
     private RecyclerView mRecyclerViewIntroduced;
     private BaseActivity mContext;
+    private int mPageNumber;
+    private String mSearchText;
+    private boolean mIsHasNextPage;
+    private boolean mIsLoading;
 
     @Override
     public void onAttach(Context context) {
@@ -65,6 +80,11 @@ public class IntroducedContestFragment extends BaseFragment implements View.OnCl
     @Override
     protected void setInitialData() {
 
+        mSearchText = "";
+        mPageNumber = 1;
+        mIsLoading = false;
+        mIsHasNextPage = false;
+
         mRecyclerViewIntroduced.setVisibility(View.VISIBLE);
         mTvDataNotAvailable.setVisibility(View.GONE);
         ArrayList<ContestResponseModel.ContestDetails> contestDetailsList = new ArrayList<>();
@@ -84,7 +104,48 @@ public class IntroducedContestFragment extends BaseFragment implements View.OnCl
 
     @Override
     public void updateUi(boolean status, int actionID, Object serviceResponse) {
+        switch (actionID) {
+            case IApiEvent.REQUEST_GET_INTRODUCED_CONTESTS_CODE:
+                if (status) {
+                    GetContestsResponseModel contestsResponseModel =
+                            (GetContestsResponseModel) serviceResponse;
+                    if (contestsResponseModel != null && contestsResponseModel.status) {
+                        LoggerUtil.d(TAG, contestsResponseModel.statusCode);
 
+                        if (contestsResponseModel.Data != null && contestsResponseModel.Data.contestList != null) {
+
+                            mTvDataNotAvailable.setVisibility(View.GONE);
+                            mRecyclerViewIntroduced.setVisibility(View.VISIBLE);
+
+                            mIsHasNextPage = !contestsResponseModel.Data.hasNextPage;
+
+                        } else {
+                            mTvDataNotAvailable.setVisibility(View.VISIBLE);
+                            mRecyclerViewIntroduced.setVisibility(View.GONE);
+                        }
+                    } else {
+                        LoggerUtil.d(TAG, getString(R.string.server_error_from_api));
+                        mTvDataNotAvailable.setVisibility(View.VISIBLE);
+                        mRecyclerViewIntroduced.setVisibility(View.GONE);
+                    }
+                } else {
+                    LoggerUtil.d(TAG, getString(R.string.status_is_false));
+                    mTvDataNotAvailable.setVisibility(View.VISIBLE);
+                    mRecyclerViewIntroduced.setVisibility(View.GONE);
+                }
+
+                mIsLoading = false;
+                break;
+
+            default:
+                LoggerUtil.d(TAG, getString(R.string.wrong_case_selection));
+                break;
+        }
+
+        if (mContext != null && !mContext.isDestroyed() && !mContext.isFinishing() && isAdded()) {
+
+            mContext.removeProgressDialog();
+        }
     }
 
     @Override
@@ -94,7 +155,49 @@ public class IntroducedContestFragment extends BaseFragment implements View.OnCl
 
     @Override
     public void getData(int actionID) {
+        if (mContext != null && !mContext.isDestroyed() && !mContext.isFinishing() && isAdded()) {
 
+            if (!ConnectivityUtils.isNetworkEnabled(mContext)) {
+                Snackbar.make(mRootView, getString(R.string.no_internet_connection), Snackbar.LENGTH_LONG).show();
+                return;
+            }
+            mContext.showProgressDialog();
+        } else {
+            return;
+        }
+
+        switch (actionID) {
+            case IApiEvent.REQUEST_GET_INTRODUCED_CONTESTS_CODE:
+
+                String trendingMirrorUrl;
+                if (mSearchText != null) {
+
+                    mSearchText = OtherUtil.replaceWithPattern(mSearchText, " ");
+                    mSearchText = mSearchText.replaceAll(" ", "%20");
+
+                    trendingMirrorUrl = IWebServices.REQUEST_GET_INTRODUCED_CONTESTS_URL + Constants.PARAM_USER_ID + "=" + SharedPrefUtils.getUserId(mContext)
+                            + "&" + Constants.PARAM_PAGE_NUMBER + "=" + String.valueOf(mPageNumber)
+                            + "&" + Constants.PARAM_SEARCH_TEXT + "=" + mSearchText;
+                } else {
+                    trendingMirrorUrl = IWebServices.REQUEST_GET_INTRODUCED_CONTESTS_URL + Constants.PARAM_USER_ID + "=" + SharedPrefUtils.getUserId(mContext)
+                            + "&" + Constants.PARAM_PAGE_NUMBER + "=" + String.valueOf(mPageNumber);
+                }
+
+                RequestManager.addRequest(new GsonObjectRequest<GetContestsResponseModel>(trendingMirrorUrl,
+                        NetworkUtil.getHeaders(mContext), null, GetContestsResponseModel.class,
+                        new VolleyErrorListener(this, actionID)) {
+
+                    @Override
+                    protected void deliverResponse(GetContestsResponseModel response) {
+                        updateUi(true, actionID, response);
+                    }
+                });
+                break;
+
+            default:
+                LoggerUtil.d(TAG, getString(R.string.wrong_case_selection));
+                break;
+        }
     }
 
     @Override
